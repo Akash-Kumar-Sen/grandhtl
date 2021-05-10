8018d907412291be064dfa910ef211db01179148
Akash Kumar Sen <akashkumarsen4@gmail.com>
2021-05-10 09:39:08 +0530

    CSS minified

--- ecom/settings.py
+++ ecom/settings.py
@@ -85,8 +85,12 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecom',
+        'USER': 'root',
+        'PASSWORD': 'CGnBAE@u^gUS',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
